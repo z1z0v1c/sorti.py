d83bf8d94a235d3c31648189abc2d47f49656139
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2024-02-11 19:07:27 +0100

    List files in working directory

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import os
 import sys
 
-
 def main():
     if (len(sys.argv) != 2):
         print("Specify the path to a single folder")
         sys.exit(1)
     else:
         try:
-            folder_path = sys.argv[1]
-            os.chdir(folder_path)
-            print(f"Changed working directory to: {folder_path}")
+            directory_path = sys.argv[1]
+            os.chdir(directory_path)
+            print(f"Changed working directory to: {directory_path}")
         except FileNotFoundError:
-            print(f"The folder '{folder_path}' does not exist.")
+            print(f"The folder '{directory_path}' does not exist.")
+        
+        contents = os.listdir(directory_path)
+
+        for item in contents:
+            print(item)
 
 
 if __name__ == "__main__":
