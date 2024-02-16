c70f432634b727b2716bc835be77d9744dac8d7d
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2024-02-16 20:08:20 +0100

    introduce separate sortify_files function

--- sorti.py
+++ sorti.py
@@ -20,10 +20,26 @@ def main():
     logging.debug(f"Source directory: {source_dir}")
     logging.debug(f"Destination directory: {dest_dir}")
     
+    sortify_files(source_dir, dest_dir)
+         
+         
+def validate_args():
+        if (len(sys.argv) != 3):
+            logging.error("Specify paths to the source and destination directories.")
+            sys.exit(1)
+
+
+def get_dirs(args):
+    if (len(args) == 2):
+        return Path(args[1]), Path(args[1])
+    else:
+        return Path(args[1]), Path(args[2])
+
+
+def sortify_files(source_dir, dest_dir):
+    change_dir(source_dir)
     make_dir(dest_dir)
     
-    change_dir(source_dir)
-        
     contents = os.listdir(source_dir)
 
     for file in contents:
@@ -33,23 +49,17 @@ def main():
             for extension in config[file_type]['extensions'].split(','):
                 
                 if file.endswith(extension):
-                    print(f"{file_type}: {file}")
                     subdir = dest_dir.joinpath(file_type) 
                     make_dir(subdir)
                     shutil.move(file, subdir.joinpath(file))
+                    logging.info(f"{file_type}: {file}")
                     for_other = False
                         
         if for_other:
             other = dest_dir.joinpath("Other")
             make_dir(other)
             shutil.move(file, other.joinpath(file))
-            print(f"Other: {file}")      
-         
-         
-def validate_args():
-        if (len(sys.argv) != 3):
-            logging.error("Specify paths to the source and destination directories.")
-            sys.exit(1)
+            logging.info(f"Other: {file}")
 
 
 def change_dir(path):
@@ -59,13 +69,6 @@ def change_dir(path):
     except FileNotFoundError:
         logging.error(f"The folder '{path}' does not exist.")
         sys.exit(1)
-
-
-def get_dirs(args):
-    if (len(args) == 2):
-        return Path(args[1]), Path(args[1])
-    else:
-        return Path(args[1]), Path(args[2])
 
 
 def make_dir(dir):
